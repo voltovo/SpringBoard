dcd4795ae1d7cd290ba164a2557cfd6db6133e24
sumanChoi <voltovo87@gmail.com>
2020-02-24 21:33:43 +0900

    refactor
    project reSetting

--- src/main/java/com/man/board/BoardApplication.java
+++ src/main/java/com/man/board/BoardApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.man.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoardApplication {
+public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBoardApplication.class, args);
+		SpringApplication.run(BoardApplication.class, args);
 	}
 
 }
--- src/test/java/com/man/board/BoardApplicationTests.java
+++ src/test/java/com/man/board/BoardApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.man.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBoardApplicationTests {
+class BoardApplicationTests {
 
 	@Test
 	void contextLoads() {
