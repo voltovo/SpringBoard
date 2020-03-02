601e0321308eed8980ba028e3587ca0001162df4
sumanChoi <voltovo87@gmail.com>
2020-03-02 21:57:39 +0900

    refactoer
    웹애플리케이션 타입 servlet 으로 변경

--- Chapter01/src/main/java/com/rubypaper/Chapter01/Chapter01Application.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01/Chapter01Application.java
@@ -1,13 +1,17 @@
 package com.rubypaper.Chapter01;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication //애플리케이션 시작 클래스를 의미
 public class Chapter01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter01Application.class, args);
+		SpringApplication application = 
+			new SpringApplication(Chapter01Application.class);
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
 	}
 
 }
