e6db1e4c78b0d50a58b4c005425c336bcc51d520
sumanChoi <voltovo87@gmail.com>
2020-03-05 19:02:27 +0900

    refactor
    객체 리턴 연습

--- Chapter01/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter01/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,5 +1,9 @@
 package com.rubypaper.controller;
 
+import java.util.Date;
+
+import com.rubypaper.domain.BoardVO;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,24 @@ public class BoardController {
     @GetMapping("/hello")
     public String hello(String name){
         return "Hello : " + name;
+    }
+
+    //객체 저장
+    @GetMapping("/getBoard")
+    public BoardVO getBoard(){
+        //BoardVO 객체 생성
+        BoardVO board = new BoardVO();
+
+        //객체 정보 설정
+        board.setSeq(1);
+        board.setTitle("테스트 제목 ...");
+        board.setWriter("테스터");
+        board.setContent("테스트 내용 입니다......");
+        board.setCreateDate(new Date());
+        board.setCnt(0);
+
+        //객체 리턴
+        return board;
     }
     
 }
