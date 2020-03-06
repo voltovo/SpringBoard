9008cd85ef6353d85628fe3bb25686b4e87e5d9a
sumanChoi <voltovo87@gmail.com>
2020-03-06 19:04:56 +0900

    refactor
    jpa 추가

--- Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
+++ Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
@@ -1,4 +1,4 @@
-package com.rubypaper.Chapter02;
+package com.rubypaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Chapter02/src/test/java/com/rubypaper/Chapter02ApplicationTests.java
+++ Chapter02/src/test/java/com/rubypaper/Chapter02ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rubypaper.Chapter02;
+package com.rubypaper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
