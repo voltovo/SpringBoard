6aabbb34073970cdd6c0d79b64979efc12712048
sumanChoi <voltovo87@gmail.com>
2020-03-11 23:01:14 +0900

    refactor
    오류 수정

--- Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
+++ Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -1,8 +1,12 @@
 package com.rubypaper;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ResourceProperties.Content;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
