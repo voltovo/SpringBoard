941f6c2ca96a6fe147d671d35b37dbc975f011af
sumanChoi <voltovo87@gmail.com>
2020-03-11 19:13:48 +0900

    feat
    chapter03 생성

---
+++ Chapter02/src/main/java/com/rubypaper/JDBCConnectionManagerRunner.java
@@ -0,0 +1,24 @@
+package com.rubypaper;
+
+import com.rubypaper.jdbc.util.JDBCConnectionManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+/**
+ * JDBCConnectionManagerRunner
+ */
+@Service
+public class JDBCConnectionManagerRunner implements ApplicationRunner {
+    @Autowired
+    private JDBCConnectionManager connectionManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("커넥션 매니저 : " + connectionManager.toString());
+        
+    }
+    
+}
