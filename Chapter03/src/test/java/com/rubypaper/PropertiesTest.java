dae3fae2399a240eae9d6e670c98377b67118cce
sumanChoi <voltovo87@gmail.com>
2020-03-11 19:39:01 +0900

    refactor
    properties를 이용한 테스트 작성

--- Chapter03/src/test/java/com/rubypaper/PropertiesTest.java
+++ Chapter03/src/test/java/com/rubypaper/PropertiesTest.java
@@ -1,7 +1,9 @@
 package com.rubypaper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 /**
  * PropertiesTest
@@ -9,9 +11,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class PropertiesTest {
 
+    @Autowired
+    Environment environment;
+
     @Test
     public void testMethod(){
-        
+        System.out.println("이름 : " + environment.getProperty("author.name"));
+        System.out.println("나이 : " + environment.getProperty("author.age"));
+        System.out.println("국가 : " + environment.getProperty("author.nation"));
     }
     
 }
