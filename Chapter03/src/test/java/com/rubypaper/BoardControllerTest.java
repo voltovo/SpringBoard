0c4050c632db35e31e3c9f4da5fd29a3bdf061b8
sumanChoi <voltovo87@gmail.com>
2020-03-12 18:28:04 +0900

    refactor
    autoConfigureMockMvc 사용하기

--- Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
+++ Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -7,13 +7,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * BoardControllerTest
  */
-@WebMvcTest
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class BoardControllerTest {
 
     @Autowired
