5df56c811751c0a06d20a80a0f404662d1caf9c9
sumanChoi <voltovo87@gmail.com>
2020-03-15 12:44:56 +0900

    feat
    컨트롤러 추가

--- Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,9 +1,10 @@
-package com.rubypaper;
+package com.rubypaper.controller;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.rubypaper.BoardVO;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
