d70a748272a4cfc1a5db591bb0d83dbd47a3fb7f
sumanChoi <voltovo87@gmail.com>
2020-03-25 21:31:25 +0900

    refactor
    table 전략 사용 위해서 수정

--- Chapter04/Chapter04/src/main/java/com/rubypaper/domain/Board.java
+++ Chapter04/Chapter04/src/main/java/com/rubypaper/domain/Board.java
@@ -6,8 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * Entity implementation class for Entity: Board
@@ -28,6 +29,7 @@ public class Board {
 	private String title;
 	private String writer;
 	private String content;
+	@Temporal(TemporalType.DATE)
 	private Date createDate;
 	private Long cnt;
 	
