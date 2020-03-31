59a83bc4a72947bdda13eeb27fd3fe8382a55df1
sumanChoi <voltovo87@gmail.com>
2020-03-31 19:11:42 +0900

    refactor
    Board 클래스 실행

--- Chapter05/src/main/java/com/rubypaper/Chapter05Application.java
+++ Chapter05/src/main/java/com/rubypaper/Chapter05Application.java
@@ -1,13 +1,17 @@
 package com.rubypaper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Chapter05Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter05Application.class, args);
+		SpringApplication application = new SpringApplication(Chapter05Application.class);
+		//내장 톰캣 구동하지 않고 실행
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
 	}
 
 }
