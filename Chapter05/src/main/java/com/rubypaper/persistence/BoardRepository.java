7809baadcad4a91b3b50e0b289726fe83f5fabe5
sumanChoi <voltovo87@gmail.com>
2020-04-02 19:45:15 +0900

    refactor
    like 쿼리 메소드 TEST

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -9,4 +9,6 @@ import com.rubypaper.domain.Board;
 public interface BoardRepository extends CrudRepository<Board, Long> {
 	//목록 검색 쿼리 메소드
 	List<Board> findByTitle(String searchKeyword);
+	//like 쿼리 메소드
+	List<Board> findByContentContaining(String searchkeyword);
 }
--- Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
@@ -19,25 +19,29 @@ public class QueryMethodTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
-	//test 메소드가 실행되기 전 데이터를 셋팅
-	@Before
-	public void dataPrepare() {
-		for (int i = 1; i <= 200; i++) {
-			Board board = new Board();
-			board.setTitle("테스트 제목 " + i);
-			board.setWriter("테스터");
-			board.setContent("테스트 내용 " + i);
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			
-			boardRepo.save(board);
-		}
-	}
+	/*
+	 * //test 메소드가 실행되기 전 데이터를 셋팅
+	 * 
+	 * @Before public void dataPrepare() { for (int i = 1; i <= 200; i++) { Board
+	 * board = new Board(); board.setTitle("테스트 제목 " + i); board.setWriter("테스터");
+	 * board.setContent("테스트 내용 " + i); board.setCreateDate(new Date());
+	 * board.setCnt(0L);
+	 * 
+	 * boardRepo.save(board); } }
+	 * 
+	 * //검색 쿼리 메소드 TEST
+	 * 
+	 * @Test public void testFindByTitle() { //테스트 제목 10인 게시글 검색 List<Board>
+	 * boardList = boardRepo.findByTitle("테스트 제목 10");
+	 * 
+	 * System.out.println("검색 결과"); for(Board board : boardList) {
+	 * System.out.println("---> " + board.toString()); } }
+	 */
 	
 	@Test
-	public void testFindByTitle() {
-		//테스트 제목 10인 게시글 검색
-		List<Board> boardList = boardRepo.findByTitle("테스트 제목 10");
+	public void testByContentContaining() {
+		//like 쿼리 메소드
+		List<Board> boardList = boardRepo.findByContentContaining("17");
 		
 		System.out.println("검색 결과");
 		for(Board board : boardList) {
