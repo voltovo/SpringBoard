ee6d844894af1b9f1e20b6217be66a7a904c333a
sumanChoi <voltovo87@gmail.com>
2020-04-02 19:28:53 +0900

    refactor
    쿼리 메소드 사용 TEST

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -1,9 +1,12 @@
 package com.rubypaper.persistence;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.rubypaper.domain.Board;
 
 public interface BoardRepository extends CrudRepository<Board, Long> {
-
+	//목록 검색 쿼리 메소드
+	List<Board> findByTitle(String searchKeyword);
 }
