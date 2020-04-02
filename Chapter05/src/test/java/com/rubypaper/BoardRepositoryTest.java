1fbb17f744477007db23ad4145633c34634bf7a7
sumanChoi <voltovo87@gmail.com>
2020-04-02 18:47:17 +0900

    refactor
    상세 조회 작성

--- Chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
+++ Chapter05/src/test/java/com/rubypaper/BoardRepositoryTest.java
@@ -18,16 +18,19 @@ public class BoardRepositoryTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
+	/*
+	 * @Test public void testInsertBoard() { Board board = new Board();
+	 * board.setTitle("첫 번째 게시글"); board.setWriter("테스터");
+	 * board.setContent("잘 등록되나요?"); board.setCreateDate(new Date());
+	 * board.setCnt(0L);
+	 * 
+	 * 
+	 * boardRepo.save(board); }
+	 */
+	
 	@Test
-	public void testInsertBoard() {
-		Board board = new Board();
-		board.setTitle("첫 번째 게시글");
-		board.setWriter("테스터");
-		board.setContent("잘 등록되나요?");
-		board.setCreateDate(new Date());
-		board.setCnt(0L);
-		
-		
-		boardRepo.save(board);
+	public void testGetBoard() {
+		Board board = boardRepo.findById(1L).get();
+		System.out.println(board.toString());
 	}
 }
