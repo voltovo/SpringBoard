f57a756c3209e56fd0fab6dfae0609102bf02a7a
sumanChoi <voltovo87@gmail.com>
2020-04-03 19:28:56 +0900

    refactor
    특정 변수 조회하기 TEST

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.rubypaper.domain.Board;
 
@@ -29,5 +30,13 @@ public interface BoardRepository extends CrudRepository<Board, Long> {
 	//위치기반 파라미터 사용하기
 	@Query("SELECT b FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
 	List<Board> queryAnnotationTest1(String searchkeyword);
+	
+	//이름 기반 파라미터 사용하기
+	@Query("SELECT b FROM Board b WHERE b.title like %:searchkeyword% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest2(@Param("searchkeyword") String searchkeyword);
+	
+	//특정 변수만 조회하기
+	@Query("SELECT b.seq, b.title, b.writer, b.createDate FROM Board b WHERE b.title like %?1% ORDER BY b.seq DESC")
+	List<Object[]> queryAnnotationTest3(@Param("searchkeyword") String searchkeyword);
 	
 }
--- Chapter05/src/test/java/com/rubypaper/QueryAnnotationTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryAnnotationTest.java
@@ -1,5 +1,6 @@
 package com.rubypaper;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -8,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.rubypaper.domain.Board;
 import com.rubypaper.persistence.BoardRepository;
 
 @RunWith(SpringRunner.class)
@@ -17,13 +17,21 @@ public class QueryAnnotationTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
+	/*
+	 * @Test public void testQueryAnnotationTest1() { List<Board> boardList =
+	 * boardRepo.queryAnnotationTest2("테스트 제목 10");
+	 * 
+	 * System.out.println("검색 결과===="); for(Board board : boardList) {
+	 * System.out.println("---> " + board.toString()); } }
+	 */
+	
 	@Test
-	public void testQueryAnnotationTest1() {
-		List<Board> boardList = boardRepo.queryAnnotationTest1("테스트 제목 10");
+	public void testQueryAnnotationTest3() {
+		List<Object[]> boardList = boardRepo.queryAnnotationTest3("테스트 제목 10");
 		
-		System.out.println("검색 결과====");
-		for(Board board : boardList) {
-			System.out.println("---> " + board.toString());
+		System.out.println("검색 결과 ===");
+		for(Object[] row : boardList) {
+			System.out.println("---> " + Arrays.toString(row));
 		}
 	}
 }
