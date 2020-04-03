253a0f70a4ccc98ed009cb511dd4fbd7b234b0fa
sumanChoi <voltovo87@gmail.com>
2020-04-03 19:14:29 +0900

    refactor
    Query 어노테이션 TEST

--- Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
+++ Chapter05/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.rubypaper.domain.Board;
@@ -24,5 +25,9 @@ public interface BoardRepository extends CrudRepository<Board, Long> {
 	 */
 	//페이징 메소드 리턴 타입 변경
 	Page<Board> findByTitleContaining(String searchkeyword, Pageable paging);
+	
+	//위치기반 파라미터 사용하기
+	@Query("SELECT b FROM Board b WHERE b.TITLE like %?1% ORDER BY b.seq DESC")
+	List<Board> queryAnnotationTest1(String searchkeyword);
 	
 }
