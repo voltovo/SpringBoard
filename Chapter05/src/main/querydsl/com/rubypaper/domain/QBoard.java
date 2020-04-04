364280756e82b924204d203d9ff0ebbfd23fafb8
sumanChoi <voltovo87@gmail.com>
2020-04-04 14:04:51 +0900

    refactor
    querydsl 설정 완료

---
+++ Chapter05/src/main/querydsl/com/rubypaper/domain/QBoard.java
@@ -0,0 +1,47 @@
+package com.rubypaper.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = -1027500891L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final NumberPath<Long> cnt = createNumber("cnt", Long.class);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.util.Date> createDate = createDateTime("createDate", java.util.Date.class);
+
+    public final NumberPath<Long> seq = createNumber("seq", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public final StringPath writer = createString("writer");
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
