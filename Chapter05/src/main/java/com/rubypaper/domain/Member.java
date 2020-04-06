02b5e4bed854432e3d1d16ed98d59f9a4db1de32
sumanChoi <voltovo87@gmail.com>
2020-04-06 19:20:00 +0900

    refactor
    member 테이블 컬럼 추가

--- Chapter05/src/main/java/com/rubypaper/domain/Member.java
+++ Chapter05/src/main/java/com/rubypaper/domain/Member.java
@@ -1,8 +1,13 @@
 package com.rubypaper.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,4 +24,9 @@ public class Member {
 	private String password;
 	private String name;
 	private String role;
+	
+	//fetch = 연관관계에 있는 게시판 정보도 같이 조회할 것인지 결정
+	//mappedBy = 연관관계의 주인을 지정 (현재 게시판 구조상 Board테이블의 member 컬럼이 연관관계의 주인)
+	@OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+	private List<Board> boardList = new ArrayList<Board>();
 }
