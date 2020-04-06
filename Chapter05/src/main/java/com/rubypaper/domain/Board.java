92ffbe1d0fec47e813fdd76e97139e7159a021a1
sumanChoi <voltovo87@gmail.com>
2020-04-06 18:48:33 +0900

    refactor
    Member 객체 다대일 매핑

--- Chapter05/src/main/java/com/rubypaper/domain/Board.java
+++ Chapter05/src/main/java/com/rubypaper/domain/Board.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -20,9 +22,15 @@ public class Board {
 	@Id @GeneratedValue
 	private Long seq;
 	private String title;
-	private String writer;
+	//private String writer;
 	private String content;
 	@Temporal(value = TemporalType.TIMESTAMP)
 	private Date createDate;
 	private Long cnt;
+	//Member 객체와 연관 매핑 처리
+	//N:1(다대일) 관계 설정
+	@ManyToOne
+	//외래키 매핑
+	@JoinColumn(name = "MEMBER_ID")
+	private Member member;
 }
