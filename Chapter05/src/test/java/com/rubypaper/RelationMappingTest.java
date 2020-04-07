dc3ed0ad33da25e8e2de63c60745283be123a66e
sumanChoi <voltovo87@gmail.com>
2020-04-07 18:28:44 +0900

    refactor
    데이터 삭제 메소드 추가

--- Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
+++ Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
@@ -33,7 +33,7 @@ public class RelationMappingTest {
 //		member1.setName("둘리");
 //		member1.setRole("User");
 //		
-//		memberRepo.save(member1);
+//		//memberRepo.save(member1);
 //		
 //		Member member2 = new Member();
 //		member2.setId("member2");
@@ -41,7 +41,7 @@ public class RelationMappingTest {
 //		member2.setName("도우너");
 //		member2.setRole("Admin");
 //		
-//		memberRepo.save(member2);
+//		//memberRepo.save(member2);
 //		
 //		for(int i = 1; i <= 3; i++) {
 //			Board board = new Board();
@@ -52,8 +52,9 @@ public class RelationMappingTest {
 //			board.setCreateDate(new Date());
 //			board.setCnt(0L);
 //			
-//			boardRepo.save(board);
+//			//boardRepo.save(board);
 //		}
+//		memberRepo.save(member1);
 //		
 //		for(int i = 1; i <= 3; i++) {
 //			Board board = new Board();
@@ -64,8 +65,9 @@ public class RelationMappingTest {
 //			board.setCreateDate(new Date());
 //			board.setCnt(0L);
 //			
-//			boardRepo.save(board);
+//			//boardRepo.save(board);
 //		}
+//		memberRepo.save(member2);
 //	}
 	
 	//게시긁 검색
@@ -81,17 +83,24 @@ public class RelationMappingTest {
 //		System.out.println("작성자 권한 : " + board.getMember().getRole());
 //	}
 	
-	@Test
-	public void testTwoWayMapping() {
-		Member member = memberRepo.findById("member1").get();
-		
-		System.out.println("=================================");
-		System.out.println(member.getName() + "가 저장한 게시글 목록");
-		System.out.println("=================================");
-		
-		List<Board> list = member.getBoardList();
-		for(Board board : list) {
-			System.out.println(board.toString());
-		}
-	}
+	//특정 회원이 적은 게시글 찾기
+//	@Test
+//	public void testTwoWayMapping() {
+//		Member member = memberRepo.findById("member1").get();
+//		
+//		System.out.println("=================================");
+//		System.out.println(member.getName() + "가 저장한 게시글 목록");
+//		System.out.println("=================================");
+//		
+//		List<Board> list = member.getBoardList();
+//		for(Board board : list) {
+//			System.out.println(board.toString());
+//		}
+//	}
+	
+	//데이터 삭제
+//	@Test
+//	public void testCascadeDelete() {
+//		memberRepo.deleteById("member1");
+//	}
 }
