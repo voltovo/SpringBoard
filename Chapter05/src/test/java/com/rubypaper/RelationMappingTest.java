362711953752d84012dec3a5927b1430bae50207
sumanChoi <voltovo87@gmail.com>
2020-04-07 18:31:14 +0900

    refactor
    영속성전이를 이용한 데이터 등록 추가

--- Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
+++ Chapter05/src/test/java/com/rubypaper/RelationMappingTest.java
@@ -24,51 +24,51 @@ public class RelationMappingTest {
 	private MemberRepository memberRepo;
 	
 	//TEST 데이터 생성
-//	@Test
-//	public void testManyToOneInsert() {
-//		
-//		Member member1 = new Member();
-//		member1.setId("member1");
-//		member1.setPassword("member111");
-//		member1.setName("둘리");
-//		member1.setRole("User");
-//		
-//		//memberRepo.save(member1);
-//		
-//		Member member2 = new Member();
-//		member2.setId("member2");
-//		member2.setPassword("member222");
-//		member2.setName("도우너");
-//		member2.setRole("Admin");
-//		
-//		//memberRepo.save(member2);
-//		
-//		for(int i = 1; i <= 3; i++) {
-//			Board board = new Board();
-//			
-//			board.setMember(member1);
-//			board.setTitle("둘리가 등록한 게시글 " + i);
-//			board.setContent("둘리가 등록한 게시글 내용 " + i);
-//			board.setCreateDate(new Date());
-//			board.setCnt(0L);
-//			
-//			//boardRepo.save(board);
-//		}
-//		memberRepo.save(member1);
-//		
-//		for(int i = 1; i <= 3; i++) {
-//			Board board = new Board();
-//			
-//			board.setMember(member2);
-//			board.setTitle("도우너가 등록한 게시글 " + i);
-//			board.setContent("도우너가 등록한 게시글 내용 " + i);
-//			board.setCreateDate(new Date());
-//			board.setCnt(0L);
-//			
-//			//boardRepo.save(board);
-//		}
-//		memberRepo.save(member2);
-//	}
+	@Test
+	public void testManyToOneInsert() {
+		
+		Member member1 = new Member();
+		member1.setId("member1");
+		member1.setPassword("member111");
+		member1.setName("둘리");
+		member1.setRole("User");
+		
+		//memberRepo.save(member1);
+		
+		Member member2 = new Member();
+		member2.setId("member2");
+		member2.setPassword("member222");
+		member2.setName("도우너");
+		member2.setRole("Admin");
+		
+		//memberRepo.save(member2);
+		
+		for(int i = 1; i <= 3; i++) {
+			Board board = new Board();
+			
+			board.setMember(member1);
+			board.setTitle("둘리가 등록한 게시글 " + i);
+			board.setContent("둘리가 등록한 게시글 내용 " + i);
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			
+			//boardRepo.save(board);
+		}
+		memberRepo.save(member1);
+		
+		for(int i = 1; i <= 3; i++) {
+			Board board = new Board();
+			
+			board.setMember(member2);
+			board.setTitle("도우너가 등록한 게시글 " + i);
+			board.setContent("도우너가 등록한 게시글 내용 " + i);
+			board.setCreateDate(new Date());
+			board.setCnt(0L);
+			
+			//boardRepo.save(board);
+		}
+		memberRepo.save(member2);
+	}
 	
 	//게시긁 검색
 //	@Test
