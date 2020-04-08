82718f999f18b80040c233651c3c9e7eb1f3d4fc
sumanChoi <voltovo87@gmail.com>
2020-04-08 18:35:18 +0900

    refactor
    Controller에 BoardService 의존성 추가

--- Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
@@ -4,33 +4,39 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rubypaper.domain.Board;
+import com.rubypaper.service.BoardService;
 
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 	
 	@RequestMapping("/getBoardList")
 	public String getBoardList(Model model) {
 		
 		List<Board> boardList = new ArrayList<Board>();
 		
-		for(int i = 1; i <= 10; i++) {
-			Board board = new Board();
-			
-			board.setSeq(new Long(i));
-			board.setTitle("게시판 프로그램 테스트");
-			board.setWriter("도우너");
-			board.setContent("게시판 프로그램 테스트입니다...");
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			
-			boardList.add(board);
-			
-		}
+		//데이터 추가 후 주석 처리
+//		for(int i = 1; i <= 10; i++) {
+//			Board board = new Board();
+//			
+//			board.setSeq(new Long(i));
+//			board.setTitle("게시판 프로그램 테스트");
+//			board.setWriter("도우너");
+//			board.setContent("게시판 프로그램 테스트입니다...");
+//			board.setCreateDate(new Date());
+//			board.setCnt(0L);
+//			
+//			boardList.add(board);
+//			
+//		}
 		
 		model.addAttribute("boardList", boardList);
 		return "getBoardList";
