930f0c330a77242130a1cc0426142f4174cd4446
sumanChoi <voltovo87@gmail.com>
2020-04-08 18:52:57 +0900

    refactor
    boardList 수정

--- Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter06/src/main/java/com/rubypaper/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rubypaper.domain.Board;
@@ -20,11 +21,12 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@RequestMapping("/getBoardList")
-	public String getBoardList(Model model) {
+	public String getBoardList(Model model, Board board) {
 		
-		List<Board> boardList = new ArrayList<Board>();
+		List<Board> boardList = boardService.getBoaList(board);
 		
 		//데이터 추가 후 주석 처리
+//		List<Board> boardList = new ArrayList<Board>();
 //		for(int i = 1; i <= 10; i++) {
 //			Board board = new Board();
 //			
@@ -41,6 +43,12 @@ public class BoardController {
 		
 		model.addAttribute("boardList", boardList);
 		return "getBoardList";
+	}
+	
+	@PostMapping("/insertBoard")
+	public String insertBoard(Board board) {
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
 	}
 	
 	@GetMapping("/insertBoard")
