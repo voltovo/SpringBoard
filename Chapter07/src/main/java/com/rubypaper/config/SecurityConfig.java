3bbb9d534be0f56d4cb9c6182fd3988ccf1ce8d2
sumanChoi <voltovo87@gmail.com>
2020-04-23 19:23:58 +0900

    refactor
    UserDetails 객체에서 인증과 인가에 필요한 정보 추출 하도록 작성

--- Chapter07/src/main/java/com/rubypaper/config/SecurityConfig.java
+++ Chapter07/src/main/java/com/rubypaper/config/SecurityConfig.java
@@ -12,9 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	//데이터 소스 의존성 주입
-	@Autowired
-	private DataSource dataSource;
+	/*
+	 * @Autowired private DataSource dataSource;
+	 */
 
+	//커스터마이징 한 userDetailsService
+	@Autowired
+	private BoardUserDetailsService boardUserDetailsService;
+	
 	@Override
 	protected void configure(HttpSecurity security) throws Exception {
 		security.authorizeRequests().antMatchers("/").permitAll();
@@ -33,6 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		security.exceptionHandling().accessDeniedPage("/accessDenied");
 		//logout 설정 추가
 		security.logout().invalidateHttpSession(true).logoutSuccessUrl("/login");
+		//UserDetails 객체에서 인증과 인가에 필요한 정보 추출
+		security.userDetailsService(boardUserDetailsService);
 	}
 	
 	/*
@@ -44,15 +51,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	 * roles("ADMIN"); }
 	 */
 	//데이터 베이스에 저장된 사용자 정보 인증
-	@Autowired
-	public void authenticate(AuthenticationManagerBuilder auth)throws Exception{
-		//비밀번호 암호화 미적용 : noop
-		String query1 = "select id username, concat('{noop}', password) password, true enabled from member where id = ?";
-		
-		String query2 = "select id, role from member where id = ?";
-		
-		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(query1).authoritiesByUsernameQuery(query2);
-
-	}
+	/*
+	 * @Autowired public void authenticate(AuthenticationManagerBuilder auth)throws
+	 * Exception{ //비밀번호 암호화 미적용 : noop String query1 =
+	 * "select id username, concat('{noop}', password) password, true enabled from member where id = ?"
+	 * ;
+	 * 
+	 * String query2 = "select id, role from member where id = ?";
+	 * 
+	 * auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(query1)
+	 * .authoritiesByUsernameQuery(query2);
+	 * 
+	 * }
+	 */
 	
 }
