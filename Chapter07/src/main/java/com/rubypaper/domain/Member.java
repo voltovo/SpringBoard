cf220219e2d82a0c699ee67affd163288f4d8fe8
sumanChoi <voltovo87@gmail.com>
2020-04-23 18:53:33 +0900

    feat
    멤버 객체 생성

---
+++ Chapter07/src/main/java/com/rubypaper/domain/Member.java
@@ -0,0 +1,26 @@
+package com.rubypaper.domain;
+
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Member {
+	
+	@Id
+	private String id;
+	private String password;
+	private String name;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	private boolean enabled;
+}
