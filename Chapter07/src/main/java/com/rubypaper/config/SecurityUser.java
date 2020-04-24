1647b2e76a83ccf795dad82ea8a1e5d34854c98f
sumanChoi <voltovo87@gmail.com>
2020-04-24 22:14:59 +0900

    refactor
    암호화 성공으로 noop 제거

--- Chapter07/src/main/java/com/rubypaper/config/SecurityUser.java
+++ Chapter07/src/main/java/com/rubypaper/config/SecurityUser.java
@@ -9,7 +9,12 @@ public class SecurityUser extends User {
 	private static final long serialVersionUID = 1L;
 	
 	public SecurityUser(Member member) {
-		super(member.getId(), "{noop}" + member.getPassword(),
+		/*
+		 * super(member.getId(), "{noop}" + member.getPassword(),
+		 * AuthorityUtils.createAuthorityList(member.getRole().toString()));
+		 */
+		//암호화 사용하기 위해서 noop 제거
+		super(member.getId(), member.getPassword(),
 				AuthorityUtils.createAuthorityList(member.getRole().toString()));
 	}
 }
