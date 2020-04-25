2c68accd3f21d042df2f851e143ffc7b6fa08768
sumanChoi <voltovo87@gmail.com>
2020-04-25 16:50:31 +0900

    refactor
    상세조회 테스트 작성

--- BoardWeb/src/test/java/com/rubypaper/BoardRepositoryTest.java
+++ BoardWeb/src/test/java/com/rubypaper/BoardRepositoryTest.java
@@ -22,39 +22,43 @@ public class BoardRepositoryTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
-	@Test
-	public void testInsert() {
+//	 테스트 데이터 만들었으므로 주석 처리
+	/*
+	 * @Test public void testInsert() {
+	 * 
+	 * Member member1 = new Member(); member1.setId("member");
+	 * member1.setPassword("member123"); member1.setName("둘리");
+	 * member1.setRole(Role.ROLE_MEMBER); member1.setEnabled(true);
+	 * memberRepo.save(member1);
+	 * 
+	 * Member member2 = new Member(); member2.setId("admin");
+	 * member2.setPassword("admin123"); member2.setName("도우너");
+	 * member2.setRole(Role.ROLE_ADMIN); member2.setEnabled(true);
+	 * memberRepo.save(member2);
+	 * 
+	 * for (int i = 0; i < 13; i++) { Board board = new Board();
+	 * board.setMember(member1); board.setTitle(member1.getName() + " 가 등록한 게시글 " +
+	 * i); board.setContent(member1.getName() + " 가 등록한 게시글 " + i);
+	 * boardRepo.save(board); }
+	 * 
+	 * for (int i = 0; i < 3; i++) { Board board = new Board();
+	 * board.setMember(member2); board.setTitle(member2.getName() + " 가 등록한 게시글 " +
+	 * i); board.setContent(member2.getName() + " 가 등록한 게시글 " + i);
+	 * boardRepo.save(board); } }
+	 */
+	 
 	
-		Member member1 = new Member();
-		member1.setId("member");
-		member1.setPassword("member123");
-		member1.setName("둘리");
-		member1.setRole(Role.ROLE_MEMBER);
-		member1.setEnabled(true);
-		memberRepo.save(member1);
+	@Test
+	//상세 조회 테스트
+	public void testGetBoard() {
 		
-		Member member2 = new Member();
-		member2.setId("admin");
-		member2.setPassword("admin123");
-		member2.setName("도우너");
-		member2.setRole(Role.ROLE_ADMIN);
-		member2.setEnabled(true);
-		memberRepo.save(member2);
+		Board board = boardRepo.findById(1L).get();
 		
-		for (int i = 0; i < 13; i++) {
-			Board board = new Board();
-			board.setMember(member1);
-			board.setTitle(member1.getName() + " 가 등록한 게시글 " + i);
-			board.setContent(member1.getName() + " 가 등록한 게시글 " + i);
-			boardRepo.save(board);
-		}
-		
-		for (int i = 0; i < 3; i++) {
-			Board board = new Board();
-			board.setMember(member2);
-			board.setTitle(member2.getName() + " 가 등록한 게시글 " + i);
-			board.setContent(member2.getName() + " 가 등록한 게시글 " + i);
-			boardRepo.save(board);
-		}
+		System.out.println("[ " + board.getSeq() + "번 게시 글 상세 정보 ]");
+		System.out.println("제목\t : " + board.getTitle());
+		System.out.println("작성자\t : " + board.getMember().getName());
+		System.out.println("내용 \t : " + board.getContent());
+		System.out.println("등록일\t : " + board.getCreateDate());
+		System.out.println("조회수 \t : " + board.getCnt());
 	}
 }
