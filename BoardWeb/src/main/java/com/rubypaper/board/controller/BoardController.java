16a688f877b3ab596ad6863e099915516bf967bf
sumanChoi <voltovo87@gmail.com>
2020-04-27 21:37:20 +0900

    refactor
    insertBoard 화면 구성

--- BoardWeb/src/main/java/com/rubypaper/board/controller/BoardController.java
+++ BoardWeb/src/main/java/com/rubypaper/board/controller/BoardController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rubypaper.board.domain.Board;
@@ -29,5 +31,16 @@ public class BoardController {
 	public String getBoard(Board board, Model model) {
 		model.addAttribute("board", boardService.getBoard(board));
 		return "board/getBoard";
+	}
+	
+	@GetMapping("/insertBoard")
+	public String insertBoardView() {
+		return "board/insertBoard";
+	}
+	
+	@PostMapping("/insertBoard")
+	public String insertBoard(Board board) {
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
 	}
 }
