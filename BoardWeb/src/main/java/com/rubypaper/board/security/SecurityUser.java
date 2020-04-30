ea199d55a5cc86df340a6bcf63fa58413c5a33b3
sumanChoi <voltovo87@gmail.com>
2020-04-30 12:28:36 +0900

    feat
    userDetail 클래스 추가

---
+++ BoardWeb/src/main/java/com/rubypaper/board/security/SecurityUser.java
@@ -0,0 +1,23 @@
+package com.rubypaper.board.security;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import com.rubypaper.board.domain.Member;
+
+public class SecurityUser extends User{
+	
+	private static final long serialVersionUID = 1L;
+	private Member member;
+	
+	public SecurityUser(Member member) {
+		super(member.getId(), "{noop}" + member.getPassword(), 
+				AuthorityUtils.createAuthorityList(member.getRole().toString()));
+		this.member = member;
+	}
+	
+	//HTML 화면에서 사용하기 위해 member 리턴
+	public Member getMember() {
+		return member;
+	}
+}
