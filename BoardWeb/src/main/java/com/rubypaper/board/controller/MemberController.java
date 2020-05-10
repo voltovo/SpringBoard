74b126715bc1840ff87b4ac22cd73b35a41adbcb
sumanChoi <voltovo87@gmail.com>
2020-05-10 21:57:55 +0900

    feat
    memeber 관리 하기 위해서 추가가

---
+++ BoardWeb/src/main/java/com/rubypaper/board/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.rubypaper.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member/")
+public class MemberController {
+
+}
