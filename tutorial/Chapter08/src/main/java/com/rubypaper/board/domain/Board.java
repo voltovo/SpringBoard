c84ea56fd6ad512bc43a485850ff59dc28698f5e
sumanChoi <voltovo@naver.com>
2022-03-26 23:32:26 +0900

    FEAT: Board class generate

---
+++ tutorial/Chapter08/src/main/java/com/rubypaper/board/domain/Board.java
@@ -0,0 +1,27 @@
+package com.rubypaper.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue
+    private Long seq;
+    private String title;
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
+    private Date createDate = new Date();
+
+    @Column(updatable = false)
+    private Long cnt = 0L;
+}
